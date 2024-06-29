16e91d59bd2a30bc68fc5218551ad6518facc33f
FiliTol <filippotolin@protonmail.com>
2024-06-29 14:22:08 +0200

    Integrated modelling code for base real scenario

--- scripts/data_definition.py
+++ scripts/data_definition.py
@@ -1,7 +1,10 @@
+import random
+
 import pandas as pd
 import json
 import re
 import pyomo.environ as pyo
+from random import sample
 
 
 def _json_to_pd(data: str) -> (pd.DataFrame, pd.DataFrame):
@@ -63,7 +66,7 @@ def _channel_cleaning(pd_object: pd.DataFrame) -> pd.DataFrame:
 
     pd_object = pd_object[
         pd.notnull(pd_object.loc[:, "node1_policy"]) & pd.notnull(pd_object.loc[:, "node2_policy"])
-    ]
+        ]
 
     return pd_object.filter(
         items=[
@@ -127,26 +130,47 @@ def _channel_directed_edges_creation(pd_object: pd.DataFrame) -> pd.DataFrame:
     :param pd_object: channels dataframe
     :return: dataframe of channels with directed relationships and features
     """
-    # Concatenate channel_id with pubkeys
-    pd_object["node1_pub"] = pd_object["channel_id"] + "-" + pd_object["node1_pub"]
-    pd_object["node2_pub"] = pd_object["channel_id"] + "-" + pd_object["node2_pub"]
 
-    # Copy dataframe and flip nodes
+    # Copy dataframe flip nodes
     pd_object1 = pd_object.copy()
+    pd_object1["channel_id"] = "INV" + pd_object1["channel_id"]
     pd_object1["node1_pub"] = pd_object["node2_pub"]
     pd_object1["node2_pub"] = pd_object["node1_pub"]
     pd_object1["node2_fee_base_msat"] = pd_object["node1_fee_base_msat"]
     pd_object1["node2_fee_rate_milli_msat"] = pd_object["node1_fee_rate_milli_msat"]
 
     pd_object = pd.concat([pd_object, pd_object1])
-    pd_object = pd_object.filter(items=["channel_id", "node1_pub", "node2_pub", "capacity", "node2_fee_base_msat", "node2_fee_rate_milli_msat"])
-    pd_object.rename(columns={"node2_fee_base_msat": "fee_base_msat", "node2_fee_rate_milli_msat": "fee_rate_milli_msat"}, inplace=True)
+    pd_object = pd_object.filter(
+        items=["channel_id", "node1_pub", "node2_pub", "capacity", "node2_fee_base_msat", "node2_fee_rate_milli_msat"])
+    pd_object.rename(
+        columns={"node2_fee_base_msat": "fee_base_msat", "node2_fee_rate_milli_msat": "fee_rate_milli_msat"},
+        inplace=True)
 
     return pd_object
 
 
-def load_and_transform(data: str) -> tuple:
+def _drop_not_connected(nodes: pd.DataFrame, channels: pd.DataFrame ) -> pd.DataFrame:
+    """
+    :param nodes: nodes dataframe
+    :param channels: channels dataframe
+    :return: filtered nodes dataframe containing only the nodes
+     that appear at least once in the channels dataframe
+    """
+    return nodes[nodes.index.isin(channels["node1_pub"])]
 
+
+def fees_conversion(pd_object: pd.DataFrame) -> pd.DataFrame:
+    """
+    :param pd_object: channels dataframe
+    :return: channels dataframe with converted fees
+    """
+    pd_object["fee_base_msat"] = pd_object["fee_base_msat"] / 1000
+    pd_object["fee_rate_milli_msat"] = pd_object["fee_rate_milli_msat"] / 1000000
+    pd_object.rename(columns={"fee_base_msat": "base_fee", "fee_rate_milli_msat": "rate_fee"}, inplace=True)
+    return pd_object
+
+
+def load_and_transform(data: str) -> tuple:
     nodes, channels = _json_to_pd(data)
 
     channels = _channel_directed_edges_creation(
@@ -158,8 +182,30 @@ def load_and_transform(data: str) -> tuple:
     ).set_index("channel_id")
 
     nodes = _node_cleaning(nodes).set_index('pub_key')
+    channels = fees_conversion(channels)
 
-    return nodes, channels
+    return _drop_not_connected(nodes, channels), channels
+
+
+def create_demand(pd_object: pd.DataFrame) -> pd.DataFrame:
+    """
+    :param pd_object: nodes dataframe
+    :return: nodes dataset with demand column
+    """
+    counterparties = sample(pd_object.index.to_list(), 2)
+    sender = counterparties[0]
+    receiver = counterparties[1]
+    random.seed(874631)
+    amount = random.randint(a=10000, b=30000)
+
+    print(
+        f"Transaction of {amount} sats from {pd_object[pd_object.index == sender]['alias'].item()} to {pd_object[pd_object.index == receiver]['alias'].item()}.")
+
+    pd_object["demand"] = 0
+    pd_object.loc[pd_object.index == sender, "demand"] = -amount
+    pd_object.loc[pd_object.index == receiver, "demand"] = amount
+
+    return pd_object
 
 
 class Channel(object):
